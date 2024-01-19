6691d4343108ec4e414f9748dc2b7d2608da9ddc
Prakash Kumar <prakash.emporio@gmail.com>
2024-01-19 18:54:57 +0530

    Add new card styles to TalentFeatureSection

--- src/components/TalentComponents/TalentFeatureSection.jsx
+++ src/components/TalentComponents/TalentFeatureSection.jsx
@@ -61,6 +61,62 @@ export default function TalentFeatureSection() {
       <h1 className="talent-feature-section-header m-5 p-5">
         Our Time-tested Process
       </h1>
+      <div className="row px-5 g-0">
+        <div className="col">
+          <div className="card ">
+            <div className="card-body talent-feature-section-card-color-1">
+              <h5 className="card-title talent-feature-section-card-header">
+                1 Position Profile
+              </h5>
+              <p className="card-text talent-feature-section-card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body talent-feature-section-card-color-2">
+              <h5 className="card-title talent-feature-section-card-header">
+                2 Research And Recruit
+              </h5>
+              <p className="card-text talent-feature-section-card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row px-5 g-0">
+        <div className="col">
+          <div className="card ">
+            <div className="card-body talent-feature-section-card-color-3">
+              <h5 className="card-title talent-feature-section-card-header">
+                1 Position Profile
+              </h5>
+              <p className="card-text talent-feature-section-card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body talent-feature-section-card-color-4">
+              <h5 className="card-title talent-feature-section-card-header">
+                2 Research And Recruit
+              </h5>
+              <p className="card-text talent-feature-section-card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
