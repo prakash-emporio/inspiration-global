47d874673f217c0518e35ac31e68a60ff12978ff
Prakash Raut <prakash_raut@outlook.com>
2024-02-20 10:55:49 +0530

    Refactor CompanyHero component and update company.css

--- src/components/CompanyComponents/CompanyHero.jsx
+++ src/components/CompanyComponents/CompanyHero.jsx
@@ -6,10 +6,19 @@ export default function CompanyHero() {
     <div className="container-fluid g-0">
       <div className="row">
         <div className="col">
-          
+          <div className="d-flex justify-content-start pt-5 px-5 align-items-center align-middle h-25 m-5">
+            <h1 className="company-hero-header">About Us</h1>
+          </div>
+          <div className="position-absolute start-50 z-2">
+            <p className="company-hero-header-two">Our Core</p>
+          </div>
         </div>
         <div className="col">
-          <img className="img-fluid" src={companyhero} alt="company-img" />
+          <img
+            className="img-fluid object-fit-fill"
+            src={companyhero}
+            alt="company-img"
+          />
         </div>
       </div>
     </div>
