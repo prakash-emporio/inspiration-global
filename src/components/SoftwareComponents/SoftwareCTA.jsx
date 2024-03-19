7cb085a9b712d90babf0efcb776218b7b4040989
Prakash Raut <prakash_raut@outlook.com>
2024-03-19 11:25:14 +0530

    Update SoftwareCTA and JobCard components

--- src/components/SoftwareComponents/SoftwareCTA.jsx
+++ src/components/SoftwareComponents/SoftwareCTA.jsx
@@ -7,14 +7,13 @@ export default function SoftwareCTA() {
         <div className="row g-0 py-5">
           <div className="col-lg-10">
             <h1 className="software-cta-title">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna
-              nulla vitae laoreet augue. Amet feugiat est integer dolor auctor
-              adipiscing nunc urna sit.
+            Some of the smartest people we have ever worked with, 
+            and they delivered beyond our expectations.
             </h1>
           </div>
           <div className="d-flex align-items-end flex-column mb-3 px-5">
-            <div className="software-cta-text-1 p-2">Name Surname</div>
-            <div className="software-cta-text-2 p-2">Title</div>
+            <div className="software-cta-text-1 p-2">- Seth Turnoff</div>
+            <div className="software-cta-text-2 p-2">Custom Medical Services</div>
           </div>
         </div>
       </div>
